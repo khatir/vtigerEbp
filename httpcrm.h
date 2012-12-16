d86639ec0be8a0fc9c207aba1682231cdfdac740
KennyTomiello <kenny.tomiello@satel.fr>
2012-12-16 13:45:15 +0100

    Enum Entity : objectTypeId utilisé par les WS pour identifier le type d'entité

--- httpcrm.h
+++ httpcrm.h
@@ -34,6 +34,40 @@ public:
     QString sessionName;
     QString userID;
 
+    /* Enum Entity : objectTypeId utilisé par les WS pour identifier le type d'entié */
+    enum Entity {
+        Campaigns	=	1	,
+        Vendors	=	2	,
+        Faq	=	3	,
+        Quotes	=	4	,
+        PurchaseOrder	=	5	,
+        SalesOrder	=	6	,
+        Invoice	=	7	,
+        PriceBooks	=	8	,
+        Calendar	=	9	,
+        Leads	=	10	,
+        Accounts	=	11	,
+        Contacts	=	12	,
+        Potentials	=	13	,
+        Products	=	14	,
+        Documents	=	15	,
+        Emails	=	16	,
+        HelpDesk	=	17	,
+        Events	=	18	,
+        Users	=	19	,
+        Groups	=	20	,
+        Currency	=	21	,
+        DocumentFolders	=	22	,
+        CompanyDetails	=	23	,
+        ServiceContracts	=	24	,
+        Services	=	25	,
+        PBXManager	=	26	,
+        Assets	=	27	,
+        ProjectMilestone	=	28	,
+        ProjectTask	=	29	,
+        Project	=	30
+      };
+
 signals:
 
 public slots:
